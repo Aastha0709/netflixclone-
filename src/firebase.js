ecf599ab11c82089546325822b802386052b07da
Aastha Malhotra <aasthamalhotra@Aasthas-MacBook-Air.local>
2023-07-17 13:33:45 +0530

    initial

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDNrpOdmYrrcgDE7tpv7t64TkMG1e7jfLE",
+    authDomain: "netflix-clone-54790.firebaseapp.com",
+    projectId: "netflix-clone-54790",
+    storageBucket: "netflix-clone-54790.appspot.com",
+    messagingSenderId: "996476699796",
+    appId: "1:996476699796:web:e81fdbfa82e3cc63d5a081"
+  };
